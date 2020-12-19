94fa6a020b65a152cfe9a47fd6dbf5b7d9a17ab8
ErickAgurto <erickagurto17@gmail.com>
2020-12-18 22:26:44 -0500

    Tercera prueba de versiones Git

--- src/Bases/main.java
+++ src/Bases/main.java
@@ -1,9 +1,10 @@
 package Bases;
+import javax.swing.JOptionPane;
 public class main {
     public static void main(String []args){
         String res;
         System.out.println("Hello World!");
         System.out.println("Primeros cambios");
-        res = JOptionPane.ShowInputDialog("Torneos de futbol");
+        res = JOptionPane.showInputDialog("Torneos de futbol");
     }
 }
