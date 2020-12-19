3be037e4a17c0fa8447394b3d029a2a7a1540c66
ErickAgurto <erickagurto17@gmail.com>
2020-12-18 22:15:15 -0500

    Segunda prueba de versiones Git

--- src/Bases/main.java
+++ src/Bases/main.java
@@ -2,5 +2,6 @@ package Bases;
 public class main {
     public static void main(String []args){
         System.out.println("Hello World!");
+        System.out.println("Primeros cambios");
     }
 }
