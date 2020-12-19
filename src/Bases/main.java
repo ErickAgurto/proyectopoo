1446c42b0b7dd447d22e027661e38983739325ac
Erick Agurto Briceño <66095247+ErickAgurto@users.noreply.github.com>
2020-12-18 22:25:03 -0500

    Update main.java
    Recibir una variable String

--- src/Bases/main.java
+++ src/Bases/main.java
@@ -1,7 +1,9 @@
 package Bases;
 public class main {
     public static void main(String []args){
+        String res;
         System.out.println("Hello World!");
         System.out.println("Primeros cambios");
+        res = JOptionPane.ShowInputDialog("Torneos de futbol");
     }
 }
