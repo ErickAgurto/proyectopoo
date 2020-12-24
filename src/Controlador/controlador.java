e91061e7aab7480ef93ccec10d58993e3a0394b8
Mujicash" <andre27012001@gmail.com>
2020-12-24 02:13:56 -0500

    prueba 1.0

--- src/Controlador/controlador.java
+++ src/Controlador/controlador.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
 
-/**
- *
- * @author andre_u88
- */
 public class controlador {
+    public static void main(String[] args) {
+        System.out.println("Pueba");
+    }
     
 }
