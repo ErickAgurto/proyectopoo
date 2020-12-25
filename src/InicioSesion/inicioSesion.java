a54cc46e910ad5699c0cfa2d682a77ac33af4814
ErickAgurto <erickagurto17@gmail.com>
2020-12-25 16:47:48 -0500

    Primeras ventanas

--- src/InicioSesion/inicioSesion.java
+++ src/InicioSesion/inicioSesion.java
@@ -1,7 +1,11 @@
 package InicioSesion;
+
+import javax.swing.JOptionPane;
+
 public class inicioSesion extends javax.swing.JFrame {
     public inicioSesion() {
         initComponents();
+        setLocationRelativeTo(null);
     }
 
     @SuppressWarnings("unchecked")
@@ -18,23 +22,23 @@ public class inicioSesion extends javax.swing.JFrame {
         cajaID = new javax.swing.JTextField();
         etiquetaContrasena = new javax.swing.JLabel();
         cajaContrasena = new javax.swing.JPasswordField();
-        botonIngreso = new javax.swing.JButton();
-        botonRegistrarse = new javax.swing.JButton();
-        etiquetaIngreseID1 = new javax.swing.JLabel();
+        botonIngresar = new javax.swing.JButton();
+        botonCerrar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
 
         panelDeInicio.setBackground(new java.awt.Color(255, 255, 255));
 
         panelLogo.setBackground(new java.awt.Color(0, 51, 102));
 
         iconoTorneo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        iconoTorneo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/soccer-ball.png"))); // NOI18N
+        iconoTorneo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/logoLogin.png"))); // NOI18N
 
         etiquetaTorneo.setFont(new java.awt.Font("Segoe UI Semibold", 1, 14)); // NOI18N
         etiquetaTorneo.setForeground(new java.awt.Color(255, 255, 255));
         etiquetaTorneo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        etiquetaTorneo.setText("LIGA PERUANA DE FUTBOL");
+        etiquetaTorneo.setText("LIGA DE FUTBOL");
 
         etiquetaBienvenida.setFont(new java.awt.Font("Segoe UI", 0, 22)); // NOI18N
         etiquetaBienvenida.setForeground(new java.awt.Color(255, 153, 0));
@@ -78,8 +82,6 @@ public class inicioSesion extends javax.swing.JFrame {
         etiquetaIngreseID.setForeground(new java.awt.Color(102, 102, 102));
         etiquetaIngreseID.setText("Ingrese ID");
 
-        cajaID.setBackground(new java.awt.Color(255, 255, 255));
-        cajaID.setText(" Ingrese su usuario");
         cajaID.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(153, 153, 153)));
         cajaID.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -91,33 +93,32 @@ public class inicioSesion extends javax.swing.JFrame {
         etiquetaContrasena.setForeground(new java.awt.Color(102, 102, 102));
         etiquetaContrasena.setText("Contraseña");
 
-        cajaContrasena.setBackground(new java.awt.Color(255, 255, 255));
         cajaContrasena.setToolTipText("");
         cajaContrasena.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(153, 153, 153)));
         cajaContrasena.setName(""); // NOI18N
 
-        botonIngreso.setBackground(new java.awt.Color(0, 51, 102));
-        botonIngreso.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        botonIngreso.setForeground(new java.awt.Color(255, 255, 255));
-        botonIngreso.setText("Ingresar");
-        botonIngreso.setBorder(null);
-        botonIngreso.setBorderPainted(false);
-        botonIngreso.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-
-        botonRegistrarse.setBackground(new java.awt.Color(255, 255, 255));
-        botonRegistrarse.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/login.png"))); // NOI18N
-        botonRegistrarse.setToolTipText("");
-        botonRegistrarse.setBorder(null);
-        botonRegistrarse.addActionListener(new java.awt.event.ActionListener() {
+        botonIngresar.setBackground(new java.awt.Color(0, 51, 102));
+        botonIngresar.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        botonIngresar.setForeground(new java.awt.Color(255, 255, 255));
+        botonIngresar.setText("Ingresar");
+        botonIngresar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        botonIngresar.setBorderPainted(false);
+        botonIngresar.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        botonIngresar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                botonRegistrarseActionPerformed(evt);
+                botonIngresarActionPerformed(evt);
             }
         });
 
-        etiquetaIngreseID1.setBackground(new java.awt.Color(255, 255, 255));
-        etiquetaIngreseID1.setFont(new java.awt.Font("Segoe UI Semilight", 1, 14)); // NOI18N
-        etiquetaIngreseID1.setForeground(new java.awt.Color(153, 153, 153));
-        etiquetaIngreseID1.setText("Regitrarse");
+        botonCerrar.setBackground(new java.awt.Color(255, 255, 255));
+        botonCerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/close.png"))); // NOI18N
+        botonCerrar.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+        botonCerrar.setFocusable(false);
+        botonCerrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonCerrarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout panelDeInicioLayout = new javax.swing.GroupLayout(panelDeInicio);
         panelDeInicio.setLayout(panelDeInicioLayout);
@@ -127,50 +128,46 @@ public class inicioSesion extends javax.swing.JFrame {
                 .addComponent(panelLogo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGroup(panelDeInicioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(panelDeInicioLayout.createSequentialGroup()
-                        .addGap(39, 39, 39)
+                        .addGap(64, 64, 64)
                         .addGroup(panelDeInicioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(etiquetaIngreseID)
-                            .addComponent(cajaID)
                             .addComponent(etiquetaContrasena)
-                            .addComponent(etiquetaPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE)
+                            .addComponent(etiquetaIngreseID)
+                            .addComponent(etiquetaPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 430, Short.MAX_VALUE)
+                            .addComponent(cajaID)
                             .addComponent(cajaContrasena)
-                            .addComponent(botonIngreso, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addContainerGap(40, Short.MAX_VALUE))
+                            .addComponent(botonIngresar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap(62, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelDeInicioLayout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(etiquetaIngreseID1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(botonRegistrarse, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18))))
+                        .addComponent(botonCerrar, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())))
         );
         panelDeInicioLayout.setVerticalGroup(
             panelDeInicioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(panelLogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(panelDeInicioLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(panelDeInicioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(botonRegistrarse, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(etiquetaIngreseID1, javax.swing.GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE))
-                .addGap(15, 15, 15)
+                .addComponent(botonCerrar, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(17, 17, 17)
                 .addComponent(etiquetaPanel)
-                .addGap(37, 37, 37)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
                 .addComponent(etiquetaIngreseID)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(cajaID, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
+                .addComponent(cajaID, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
                 .addComponent(etiquetaContrasena)
                 .addGap(18, 18, 18)
                 .addComponent(cajaContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(botonIngreso, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(65, Short.MAX_VALUE))
+                .addGap(47, 47, 47)
+                .addComponent(botonIngresar, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(55, 55, 55))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelDeInicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(panelDeInicio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -184,9 +181,24 @@ public class inicioSesion extends javax.swing.JFrame {
         // codigos por agregar
     }//GEN-LAST:event_cajaIDActionPerformed
 
-    private void botonRegistrarseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonRegistrarseActionPerformed
-        // codigos por agregar
-    }//GEN-LAST:event_botonRegistrarseActionPerformed
+    private void botonCerrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonCerrarActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_botonCerrarActionPerformed
+
+    private void botonIngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonIngresarActionPerformed
+        String usuarioCorrecto = "admin";
+        String contrasenaCorrecta = "proyecto";
+        if(usuarioCorrecto.equals(cajaID.getText())){
+            String contrasena ="";
+            for(int i = 0 ; i < cajaContrasena.getPassword().length; i++)
+                contrasena += cajaContrasena.getPassword()[i];
+            if(contrasenaCorrecta.equals(contrasena))
+                JOptionPane.showMessageDialog(null, "Bienvenido");
+            else
+                JOptionPane.showMessageDialog(null, "Contraseña Incorrecta", "Error", JOptionPane.ERROR_MESSAGE);
+        }else
+            JOptionPane.showMessageDialog(null, "Usuario Desconocido", "Error", JOptionPane.ERROR_MESSAGE);
+    }//GEN-LAST:event_botonIngresarActionPerformed
 
     public static void main(String args[]) {
         
@@ -198,14 +210,13 @@ public class inicioSesion extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton botonIngreso;
-    private javax.swing.JButton botonRegistrarse;
+    private javax.swing.JButton botonCerrar;
+    private javax.swing.JButton botonIngresar;
     private javax.swing.JPasswordField cajaContrasena;
     private javax.swing.JTextField cajaID;
     private javax.swing.JLabel etiquetaBienvenida;
     private javax.swing.JLabel etiquetaContrasena;
     private javax.swing.JLabel etiquetaIngreseID;
-    private javax.swing.JLabel etiquetaIngreseID1;
     private javax.swing.JLabel etiquetaPanel;
     private javax.swing.JLabel etiquetaTorneo;
     private javax.swing.JLabel iconoTorneo;
---
+++ src/Ventanas/ventClub.java
@@ -0,0 +1,165 @@
+package Ventanas;
+public class ventClub extends javax.swing.JFrame {
+
+    public ventClub() {
+        initComponents();
+        setLocationRelativeTo(null);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelJugador = new javax.swing.JPanel();
+        botonCerrar = new javax.swing.JButton();
+        panelLogo = new javax.swing.JPanel();
+        iconoTorneo = new javax.swing.JLabel();
+        etiquetaTorneo = new javax.swing.JLabel();
+        etiquetaBienvenida = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        panelJugador.setBackground(new java.awt.Color(255, 255, 255));
+
+        botonCerrar.setBackground(new java.awt.Color(255, 255, 255));
+        botonCerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/close.png"))); // NOI18N
+        botonCerrar.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+        botonCerrar.setFocusable(false);
+        botonCerrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonCerrarActionPerformed(evt);
+            }
+        });
+
+        panelLogo.setBackground(new java.awt.Color(0, 51, 102));
+
+        iconoTorneo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        iconoTorneo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/logoClub.png"))); // NOI18N
+
+        etiquetaTorneo.setFont(new java.awt.Font("Segoe UI Semibold", 1, 14)); // NOI18N
+        etiquetaTorneo.setForeground(new java.awt.Color(255, 255, 255));
+        etiquetaTorneo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        etiquetaTorneo.setText("LIGA DE FÚTBOL");
+
+        etiquetaBienvenida.setFont(new java.awt.Font("Segoe UI", 0, 22)); // NOI18N
+        etiquetaBienvenida.setForeground(new java.awt.Color(255, 153, 0));
+        etiquetaBienvenida.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        etiquetaBienvenida.setText("Menú Club");
+
+        javax.swing.GroupLayout panelLogoLayout = new javax.swing.GroupLayout(panelLogo);
+        panelLogo.setLayout(panelLogoLayout);
+        panelLogoLayout.setHorizontalGroup(
+            panelLogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(etiquetaTorneo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(panelLogoLayout.createSequentialGroup()
+                .addGroup(panelLogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelLogoLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(etiquetaBienvenida, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(panelLogoLayout.createSequentialGroup()
+                        .addGap(54, 54, 54)
+                        .addComponent(iconoTorneo)
+                        .addGap(0, 52, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        panelLogoLayout.setVerticalGroup(
+            panelLogoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelLogoLayout.createSequentialGroup()
+                .addGap(90, 90, 90)
+                .addComponent(iconoTorneo)
+                .addGap(30, 30, 30)
+                .addComponent(etiquetaBienvenida)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(etiquetaTorneo, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(130, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout panelJugadorLayout = new javax.swing.GroupLayout(panelJugador);
+        panelJugador.setLayout(panelJugadorLayout);
+        panelJugadorLayout.setHorizontalGroup(
+            panelJugadorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelJugadorLayout.createSequentialGroup()
+                .addComponent(panelLogo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 521, Short.MAX_VALUE)
+                .addComponent(botonCerrar)
+                .addContainerGap())
+        );
+        panelJugadorLayout.setVerticalGroup(
+            panelJugadorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelJugadorLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(botonCerrar)
+                .addContainerGap(414, Short.MAX_VALUE))
+            .addComponent(panelLogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelJugador, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelJugador, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void botonCerrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonCerrarActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_botonCerrarActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ventClub.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ventClub.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ventClub.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ventClub.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ventClub().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botonCerrar;
+    private javax.swing.JLabel etiquetaBienvenida;
+    private javax.swing.JLabel etiquetaTorneo;
+    private javax.swing.JLabel iconoTorneo;
+    private javax.swing.JPanel panelJugador;
+    private javax.swing.JPanel panelLogo;
+    // End of variables declaration//GEN-END:variables
+}
